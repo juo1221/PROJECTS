d0c63391f23fdbe68e0e9addafa9339132ee2612
juo <hwgiwh271@gmail.com>
2021-03-25 02:38:31 +0900

    JS fix code
    fixed the code more simple for changing the color when clicking button than before

--- mini game practiece/main.js
+++ mini game practiece/main.js
@@ -1,4 +1,5 @@
 "use strict";
+const container = document.querySelector(".items");
 
 function loadItems() {
   return fetch("data/data.json")
@@ -7,8 +8,6 @@ function loadItems() {
 }
 
 function displayItems(items) {
-  const container = document.querySelector(".items");
-  console.log(items.map((item) => createHTMLString(item)).join(""));
   container.innerHTML = items.map((item) => createHTMLString(item)).join("");
 }
 
@@ -20,8 +19,8 @@ function createHTMLString(item) {
   </li>`;
 }
 
-function onButtonItem(event, items) {
-  const dataset = event.target.dataset;
+function onButtonItem(e) {
+  const dataset = e.target.dataset;
   const key = dataset.key;
   const value = dataset.value;
 
@@ -29,47 +28,12 @@ function onButtonItem(event, items) {
     return;
   } else {
     updateItems(key, value);
-
-    switch (value) {
-      case "blue":
-        const bacColorblue = document.querySelector(".items");
-        bacColorblue.classList.remove(
-          "colorChange",
-          "coloryellow",
-          "colorpink"
-        );
-
-        bacColorblue.classList.add("colorblue");
-        return;
-      case "yellow":
-        const bacColoryellow = document.querySelector(".items");
-        bacColoryellow.classList.remove(
-          "colorChange",
-          "colorblue",
-          "colorpink"
-        );
-        bacColoryellow.classList.add("coloryellow");
-        return;
-      case "pink":
-        const bacColorpink = document.querySelector(".items");
-        bacColorpink.classList.remove(
-          "colorChange",
-          "colorblue",
-          "coloryellow"
-        );
-        bacColorpink.classList.add("colorpink");
-        return;
-    }
+    changeColor(e);
   }
 }
 function updateItems(key, value) {
   const allitems = document.querySelectorAll(".item");
   allitems.forEach((item) => {
-    console.log(`key: ${key}`);
-    console.log(`item.dataset: ${item.dataset}`);
-    console.log(`item.dataset[key]: ${item.dataset[key]}`);
-    console.log(`value: ${value}`);
-
     if (item.dataset[key] === value) {
       item.classList.remove("invisible");
     } else {
@@ -78,10 +42,15 @@ function updateItems(key, value) {
   });
 }
 
+function changeColor(e) {
+  console.log(e.target);
+  const color = e.target.dataset.color;
+  container.style.backgroundColor = color;
+}
+
 function onBgColorChange(items) {
-  const bgColor = document.querySelector(".items");
-  bgColor.classList.remove("colorpink", "colorblue", "coloryellow");
-  bgColor.classList.add("colorChange");
+  container.style.backgroundColor = "transparent";
+
   displayItems(items);
 }
 
@@ -89,7 +58,7 @@ function setEventListener(items) {
   const logo = document.querySelector(".home__logo");
   const button = document.querySelector("#buttons");
   logo.addEventListener("click", () => onBgColorChange(items));
-  button.addEventListener("click", (event) => onButtonItem(event, items));
+  button.addEventListener("click", (e) => onButtonItem(e));
 }
 
 loadItems()
